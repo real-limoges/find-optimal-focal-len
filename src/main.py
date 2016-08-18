541be3341de3acdd23a632a67e5536c46ef90f46
Real Limoges <b.real.limoges@gmail.com>
2016-08-17 21:54:20 -0700

    update mistakes; workaround pagination

--- src/main.py
+++ src/main.py
@@ -7,7 +7,15 @@ with open('/Users/reallimoges/.flickr/key') as f:
 with open('/Users/reallimoges/.flickr/secret_key') as f:
   secret = f.read().strip()
 
-flickr = flickapi.FlickrAPI(key, secret, cache=True)
+flickr = flickrapi.FlickrAPI(key, secret, cache=True)
+
+def find_page_nums(user_id):
+  photo_set = flickr.photos.search(user_id=user_id).find('photos')
+  return int(photo_set.get('pages'))
 
 if __name__ == '__main__':
-  pass
+
+  # My flickr user name - this is public.
+  user_id = '134191181@N04'
+  print find_page_nums(user_id)
+  print type(find_page_nums(user_id))
